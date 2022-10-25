998de274db9709fae27c087ced87c99d916bc8ae
yang <1145938287@qq.com>
2022-10-25 15:40:53 +0800

    userlist

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import App from "./App.vue";
 import router from "./router";
-
+import locale from 'element-plus/lib/locale/lang/zh-cn';
 import "./assets/main.css";
-
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
 
+app.use(ElementPlus,{locale})
 app.use(ElementPlus);
 app.use(createPinia());
 app.use(router);
--- src/views/power/UserList.vue
+++ src/views/power/UserList.vue
@@ -1,5 +1,172 @@
 <template>
-    <div>
-        这是userlist
-    </div>
+  <div class="user-list">
+    <h1 class="mb-20">用户列表</h1>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column align="center" prop="username" label="头像" width="140" />
+      <el-table-column align="center" prop="username" label="用户名" width="160" />
+      <el-table-column align="center" prop="avatarname" label="昵称" width="160" />
+      <el-table-column align="center" prop="id" label="用户id" width="150" />
+      <el-table-column align="center" prop="phoneNum" label="手机号" width="auto" />
+      <el-table-column align="center" fixed="right" label="操作" width="250">
+        <template #default>
+          <el-button link type="primary" size="small" @click="handleClick">修改角色</el-button>
+          <el-button link type="primary" size="small" @click="handleClick2">删除用户</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination small background layout="total, sizes, prev, pager, next, jumper" :total="50" class="mt-20" />
+  </div>
 </template>
+<script lang="ts" setup>
+const tableData = [
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+  {
+    username: "小明",
+    avatarname: "",
+    id: "",
+    phoneNum: "110",
+  },
+];
+const handleClick = function () {
+  console.log("click");
+};
+const handleClick2 = function () {
+  console.log("click2");
+};
+</script>
+<style scoped>
+.user-list {
+  padding: 20px;
+}
+</style>
