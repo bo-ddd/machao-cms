97fdc31df8376aaa7145c74450411293a8ca4456
fuchenzijian <2014873253@qq.com>
2022-10-27 09:35:06 +0800

    update loginView,update cofig

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,11 @@ export default defineConfig({
   },
   server:{
     proxy:{
-      '/api': 'http://localhost:3000',
+      '/api': {
+        target: 'http://127.0.0.1:7001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
     }
   }
 });
